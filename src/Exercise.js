b517ee9b2582a046d04f6537877045a5c16733e2
Mush <malina.musuroi@yahoo.com>
2019-05-09 22:13:14 +0100

    Implement error checking and visitor for expressions

--- src/Exercise.js
+++ src/Exercise.js
@@ -1,62 +1,55 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
-import  './Exercise.css'
+import './Exercise.css'
 import { Switch, Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import {parse} from "./parser"
-import JSONTree from 'react-json-tree'
+import { parse } from "./parser"
+import { parse as expressionParse } from "./expressionParser"
 import visit from "./visitor"
+import visitWithExpression from "./visitWithExpression"
 import MonacoEditor from 'react-monaco-editor';
-
 
 class Exercise extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      code: '// type your code...',
       value: '',
       data: '',
       testValue: '',
       example: '',
       exampleValue: '',
-      ast: '',
       checkValue: '',
-      checkResult: ''
+      editor: {},
+      badInput: ''
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleExample = this.handleExample.bind(this);
-    this.handleValue = this.handleValue.bind(this);
-    this.handleExample = this.handleExample.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onSubmitAST = this.onSubmitAST.bind(this);
-    this.onCheckIfSame = this.onCheckIfSame.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-  }
-  
-  handleChange(input) {
-    console.log("Input value: " + input.target.value)
-    this.setState({ value: input.target.value })
   }
 
-  handleExample(input) {
+  handleChange = (val) => {
+    this.setState({ value: val })
+  }
+
+  handleExample = (input) => {
     this.setState({ exampleValue: input.target.value })
   }
 
-  onExampleSubmit = () =>  {
-      if(this.state.value2 !== 'False') {
-        this.setState({ example: "Wrong answer" })
-      } else if(this.state.value2 !== 'True')  {
-        this.setState({ example: "Correct answer" })
-      } else {
-        this.setState({ example: " " })
-      }
+  onExampleSubmit = () => {
+    if (this.state.value2 !== 'False') {
+      this.setState({ example: "Wrong answer" })
+    } else if (this.state.value2 !== 'True') {
+      this.setState({ example: "Correct answer" })
+    } else {
+      this.setState({ example: " " })
+    }
   }
 
-  handleInput(input) {
+  handleInput = (input) => {
     this.setState({ checkValue: input.target.value })
   }
 
-  handleValue(input) {
-    console.log("Input value: " + input.target.value)
+  handleBadInput = (input) => {
+    this.setState({ badInput: input.target.value })
+  }
+
+  handleValue = (input) => {
     this.setState({ testValue: input.target.value })
   }
 
@@ -65,7 +58,7 @@ class Exercise extends Component {
     var testInput = this.state.testValue
     fetch('/compile', {
       method: 'POST',
-      body: JSON.stringify({ val: inputValue, v: testInput}),
+      body: JSON.stringify({ val: inputValue, v: testInput }),
       headers: {
         "Content-Type": "application/json",
       }
@@ -73,31 +66,53 @@ class Exercise extends Component {
       .catch(error => console.log(error));
   }
 
-  onSubmitAST() {
-    var ast = parse(this.state.value);
-    var array = JSON.stringify(ast);
-    this.setState({ ast })
+  onCheckIfSame = () => {
+    let errors = visit(parse(this.state.value)[0], parse(this.state.checkValue)[0], [], []);
+    const instructorErrors = visitWithExpression(parse(this.state.value)[0], expressionParse(this.state.badInput), [])
+      .filter(error => error != null);
+
+    errors = errors.concat(instructorErrors);
+    this.setState({ compilerErrors: errors });
+    const {
+      editor,
+      decorations: previousDecorations = []
+    } = this.state;
+    var decorations = editor.deltaDecorations(previousDecorations, errors.map(error => (
+      {
+        range: new monaco.Range(error.lineNumber, error.startPosition, error.lineNumber, error.endPosition),
+        options: { inlineClassName: 'myInlineDecoration' }
+      }
+    )));
+    this.setState({ decorations });
   }
 
-  onCheckIfSame() {
-    console.log(parse(this.state.value));
-    let value = visit(parse(this.state.value), parse(this.state.checkValue));
-    this.setState({checkResult: "" + value})
-    console.log("Are the values the same??????" + value)
-  }
-
-  editorDidMount(editor, monaco) {
-    console.log('editorDidMount', editor);
+  editorDidMount = (editor) => {
     editor.focus();
+    this.setState({ editor });
   }
-  onChangeee(newValue, e) {
-    console.log('onChange', newValue, e);
-  }
+
+  renderErrors = () => {
+    const formatDiagnostic = ({ lineNumber, startPosition, message }) => {
+      return `${lineNumber}:${startPosition}: ${message}\n`;
+    };
+
+    return (
+      <div>
+        <button onClick={() => this.setState({ isShowingErrors: true })}>Show errors</button>
+        {this.state.isShowingErrors &&
+          <pre>{this.state.compilerErrors.map(formatDiagnostic)}</pre>
+        }
+      </div>
+    )
+  };
 
   render() {
-    const code = this.state.code;
     const options = {
-      selectOnLineNumbers: true
+      selectOnLineNumbers: true,
+      glyphMargin: true,
+      minimap: {
+        enabled: false
+      }
     };
     return (
       <div>
@@ -105,30 +120,27 @@ class Exercise extends Component {
         <p>Write a function which returns true if a list contains at least a duplicate</p>
         <b> Given the input [1, 2, 3] what is the output of your program? </b>
         <textarea className="example-area" align="top" type="text" id="name" name="name" value={this.state.exampleValue} onChange={this.handleExample} />
-        <button  onClick={this.onExampleSubmit}> Submit </button>
-         {this.state.example}
-        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px'}}>
-         <textarea align="top" type="text" id="name" name="name" className="code-area" value={this.state.value} onChange={this.handleChange} />
-         {this.state.checkResult !=="" && <span style={{backgroundColor:"red", textEmphasisColor: "white"}}>{this.state.checkResult}</span>}
-          <div style={{marginRight: '800px'}}>
+        <button onClick={this.onExampleSubmit}> Submit </button>
+        {this.state.example}
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
+          <MonacoEditor width="500" height="400" language="haskell" theme="vs-dark" value={this.state.value} options={options} onChange={this.handleChange} editorDidMount={this.editorDidMount} ref={(r) => this.r = r} />
+          <div style={{ marginRight: '800px' }}>
             <p> Test your program: </p>
             <textarea align="top" type="text" id="name" name="name" className="test-area" value={this.state.testValue} onChange={this.handleValue} />
-           </div>
-         </div>
-         <div style={{marginRight: '800px'}}>
-            <p> Check if same as sample solution: </p>
-            <textarea align="top" type="text" id="name" name="name" className="code-area" value={this.state.checkValue} onChange={this.handleInput} />
-         </div>
-         <div className="result"> CheckResult: {this.state.checkResult} </div>
+          </div>
+        </div>
+        <div style={{ marginRight: '800px' }}>
+          {this.renderErrors()}
+          <p> Check if same as sample solution: </p>
+          <textarea align="top" type="text" id="name" name="name" className="code-area" value={this.state.checkValue} onChange={this.handleInput} />
+          <textarea align="top" type="text" className="code-area" value={this.state.badInput} onChange={this.handleBadInput} />
+        </div>
         <div style={{ marginTop: '5px', marginLeft: '270px' }} >
           <button onClick={this.onSubmit}> Submit </button>
         </div>
         <div className="result"> Result: {this.state.data} </div>
-
-        <div className="result"> AST: <JSONTree data={this.state.ast}/></div>
-        <button onClick={this.onSubmitAST}> See AST </button>
         <button onClick={this.onCheckIfSame}> Check value </button>
-        <Link to="/" style={{display: 'block', textAlign: 'center'}}>Previous</Link>
+        <Link to="/" style={{ display: 'block', textAlign: 'center' }}>Previous</Link>
       </div>
     )
   }
